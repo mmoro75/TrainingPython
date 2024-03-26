94811e2d59191105db2c962bb9dd22da21bf4180
Marco Moro <35698401+mmoro75@users.noreply.github.com>
2024-03-26 11:08:03 +0000

    Update Exercise_File_3.py
    changes after teacher review

--- Python_Exercise_Files/Exercise_File_3.py
+++ Python_Exercise_Files/Exercise_File_3.py
@@ -1,7 +1,7 @@
 ## given the below list:
 
 numbers=[1,2,4,5,6,7,8,11,14,15,17,18,20]
-
+new_list=[]
 # using the for loop and the conditional statement create a new list empty list and include only the even numbers in it
 
 for n in numbers:
