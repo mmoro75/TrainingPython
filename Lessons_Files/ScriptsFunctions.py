ca4e88812f1162890baaead816156d9cc0ceb2df
Moro <marco.moro@refinitiv.com>
2024-05-23 15:06:42 +0100

    missing ScriptFunctions for Function Lessons File

---
+++ Lessons_Files/ScriptsFunctions.py
@@ -0,0 +1,35 @@
+
+
+
+
+def calculator(a,b):
+    result=a-b
+    print(result)
+    return None
+
+def main():
+     a=3
+     b=5
+     calculator(a,b)
+
+if __name__=="__main__":
+    main()
+    
+"""
+
+
+def calculator(a,b):
+    result=a-b
+    print(result)
+    return None
+
+
+a=3
+b=5
+calculator(a,b)
+"""
+
+
+
+
+
